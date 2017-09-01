1f317036b05b6b0b38811115476bf3ac48d9e352
Juan Sebastián Cely <juanscelyg@gmail.com>
2017-09-01 17:59:12 +0200

    Code to control VREP using AWS
    Add the code to control the VREP using AWS, but without specific frame

--- agente_robot.py
+++ agente_robot.py
@@ -14,6 +14,42 @@ except:
 
 AWS_updater.init(1)
 RS485.init('/dev/ttyACM0', 9600)
+
+print ('Program started')
+vrep.simxFinish(-1) # just in case, close all opened connections
+clientID=vrep.simxStart('192.168.4.104',19999,True,True,5000,5) # Connect to V-REP
+if clientID!=-1:
+    print ('Connected to remote API server')
+
+    # Now try to retrieve data in a blocking fashion (i.e. a service call):
+    res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_blocking)
+    if res==vrep.simx_return_ok:
+        print ('Number of objects in the scene: ',len(objs))
+    else:
+        print ('Remote API function call returned with error code: ',res)
+
+    time.sleep(2)
+
+    # Now send some data to V-REP in a non-blocking fashion:
+    vrep.simxAddStatusbarMessage(clientID,'Simulating . . .',vrep.simx_opmode_oneshot)
+
+	# Here into the main code to move the robot
+    errorCode,q1_motor_handle=vrep.simxGetObjectHandle(clientID,'MURA_joint3',vrep.simx_opmode_oneshot_wait)
+    print errorCode
+    vrep.simxSetJointTargetPosition(clientID,q1_motor_handle,45,vrep.simx_opmode_oneshot_wait)
+
+    # Now send some data to V-REP in a non-blocking fashion:
+    vrep.simxAddStatusbarMessage(clientID,'Bye!',vrep.simx_opmode_oneshot)
+
+    # Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+    vrep.simxGetPingTime(clientID)
+
+    # Now close the connection to V-REP:
+    vrep.simxFinish(clientID)
+else:
+    print ('Failed connecting to remote API server')
+
+### While loop to received info
 try:
 	while True:
 		pass
@@ -21,40 +57,5 @@ try:
 except KeyboardInterrupt:
 	RS485.close()
 	exit()
-#'''
-#print ('Program started')
-#vrep.simxFinish(-1) # just in case, close all opened connections
-#clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to V-REP
-#if clientID!=-1:
-    #print ('Connected to remote API server')
 
-    ## Now try to retrieve data in a blocking fashion (i.e. a service call):
-    #res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_blocking)
-    #if res==vrep.simx_return_ok:
-        #print ('Number of objects in the scene: ',len(objs))
-    #else:
-        #print ('Remote API function call returned with error code: ',res)
-
-    #time.sleep(2)
-
-    ## Now send some data to V-REP in a non-blocking fashion:
-    #vrep.simxAddStatusbarMessage(clientID,'Simulating . . .',vrep.simx_opmode_oneshot)
-
-	## Here into the main code to move the robot
-    #errorCode,q1_motor_handle=vrep.simxGetObjectHandle(clientID,'MURA_joint3',vrep.simx_opmode_oneshot_wait)
-    #print errorCode
-    #vrep.simxSetJointTargetPosition(clientID,q1_motor_handle,45,vrep.simx_opmode_oneshot_wait)
-
-    ## Now send some data to V-REP in a non-blocking fashion:
-    #vrep.simxAddStatusbarMessage(clientID,'Bye!',vrep.simx_opmode_oneshot)
-
-    ## Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
-    #vrep.simxGetPingTime(clientID)
-
-    ## Now close the connection to V-REP:
-    #vrep.simxFinish(clientID)
-#else:
-    #print ('Failed connecting to remote API server')
-#print ('Program ended')
-#'''
 
